0f20dd87779d5d8f2fee66034c9b695392bf565a
Gustavo Barbieri <gbarbieri@segware.com>
2022-05-29 20:49:31 -0300

    Improving Behavior

--- android/src/main/java/com/lg2/eddystone/EddystoneModule.java
+++ android/src/main/java/com/lg2/eddystone/EddystoneModule.java
@@ -11,6 +11,7 @@
 
 package com.lg2.eddystone;
 
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -23,13 +24,15 @@ import android.bluetooth.*;
 import android.bluetooth.le.*;
 import android.os.ParcelUuid;
 import android.content.Context;
+import android.os.Build;
+import android.util.Log;
 
 import java.util.List;
 import java.util.ArrayList;
 
 import com.facebook.react.HeadlessJsTaskService;
 
-public class EddystoneModule extends ReactContextBaseJavaModule {
+public class EddystoneModule extends ReactContextBaseJavaModule implements LifecycleEventListener{
   /** @property {ReactApplicationContext} The react app context */
   private final ReactApplicationContext reactContext;
 
@@ -60,6 +63,8 @@ public class EddystoneModule extends ReactContextBaseJavaModule {
   /** @property byte Empty frame type byte identifier */
   public static final byte FRAME_TYPE_EMPTY = 0x40;
 
+  public boolean isClosed = false;
+
   /**
    * EddystoneModule class constructor
    *
@@ -70,8 +75,17 @@ public class EddystoneModule extends ReactContextBaseJavaModule {
   public EddystoneModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    reactContext.addLifecycleEventListener(this);
     bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
   }
+
+   public boolean isClosed() {
+        return isClosed;
+    }
+
+    public void setIsClosed(boolean closed) {
+        isClosed = closed;
+    }
 
   /**
    * Returns the name of the module
@@ -93,13 +107,16 @@ public class EddystoneModule extends ReactContextBaseJavaModule {
    * @public
    */
   private void emit(String event, Object params) {
-    if(event == "onUIDFrame" && params.toString().contains("ffffffff")) {
-      Context context = reactContext.getApplicationContext();
-        Intent myIntent = new Intent(context, EddystoneEventService.class);
-        context.startService(myIntent);
-        HeadlessJsTaskService.acquireWakeLockNow(context);
-    }
     reactContext.getJSModule(RCTDeviceEventEmitter.class).emit(event, params);
+  }
+
+  private void emitHeadlessEvent(String event, Object params) {
+    if(isClosed() && event == "onUIDFrame" && params.toString().contains("ffffffff")) {
+      Context context = reactContext.getApplicationContext();
+      Intent myIntent = new Intent(context, EddystoneEventService.class);
+      context.startService(myIntent);
+      HeadlessJsTaskService.acquireWakeLockNow(context);
+    }
   }
 
   /**
@@ -241,7 +258,11 @@ public class EddystoneModule extends ReactContextBaseJavaModule {
         params.putInt("rssi", result.getRssi());
 
         // dispatch event
-        emit(event, params);
+        if (isClosed()){
+          emitHeadlessEvent(event, params);
+        } else {
+          emit(event, params);
+        }
       } else if (frameType == FRAME_TYPE_URL) {
 
         // build the url from the frame's bytes
@@ -279,6 +300,21 @@ public class EddystoneModule extends ReactContextBaseJavaModule {
       }
     }
   };
+
+  @Override
+  public void onHostResume() {
+      this.setIsClosed(false);
+  }
+
+  @Override
+  public void onHostPause() {
+      this.setIsClosed(false);
+  }
+
+  @Override
+  public void onHostDestroy() {
+      this.setIsClosed(true);
+  }
 
   /**
    * Starts scanning for Eddystone beacons
@@ -318,15 +354,5 @@ public class EddystoneModule extends ReactContextBaseJavaModule {
   public void stopScanning() {
     scanner.stopScan(scanCallback);
     scanner = null;
-  }
-
-  @ReactMethod
-  public void startService() {
-    this.reactContext.startService(new Intent(this.reactContext, EddystoneService.class));
-  }
-
-  @ReactMethod
-  public void stopService() {
-    this.reactContext.stopService(new Intent(this.reactContext, EddystoneService.class));
   }
 }
