68522e4aa998bfa936db846690a39aed60c4374f
Gustavo Barbieri <gbarbieri@segware.com>
2022-09-09 14:02:37 -0300

    Changing Property Name

--- android/src/main/java/com/lg2/eddystone/EddystoneModule.java
+++ android/src/main/java/com/lg2/eddystone/EddystoneModule.java
@@ -203,7 +203,7 @@ public class EddystoneModule extends ReactContextBaseJavaModule {
         serviceData = result.getScanRecord().getServiceData(CONFIGURATION_UUID);
 
         WritableMap newParams = Arguments.createMap();
-        newParams.putString("onUIDFrame", "Connecting beacon");
+        newParams.putString("error", "Connecting beacon");
 
         emit("onUIDFrame", newParams);
 
